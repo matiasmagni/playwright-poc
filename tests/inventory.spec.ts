5b6c6170ad61ad3c326cc127ee9b20728ec85c6e
Matias J. Magni <matias.magni@gmail.com>
2025-03-14 10:30:48 -0300

    Added 'Should decrease products count in shopping cart badge when removing' test to Inventory test suite.

--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { InventoryPage } from './pages/InventoryPage';
 
 test.describe('Inventory Test Suite', () => {
-  test('Should show products count in shopping cart badge', async ({ page }) => {
+  test('Should increase products count in shopping cart badge when adding', async ({ page }) => {
     const inventoryPage = new InventoryPage(page);
     await inventoryPage.goto();
     expect(await inventoryPage.getShoppingCartBadgeCount()).toBe(0);
@@ -11,6 +11,24 @@ test.describe('Inventory Test Suite', () => {
       expect(await inventoryPage.getInventoryItem(i)).toBeVisible();
       await (await inventoryPage.getAddToCartButton(i)).click();
       expect(await inventoryPage.getShoppingCartBadgeCount()).toBe(i + 1);
+    }
+  });
+  
+  test('Should decrease products count in shopping cart badge when removing', async ({ page }) => {
+    const inventoryPage = new InventoryPage(page);
+    await inventoryPage.goto();
+    expect(await inventoryPage.getShoppingCartBadgeCount()).toBe(0);
+
+    // Add products to the cart
+    for (let i = 0; i < 6; i++) {
+      await (await inventoryPage.getAddToCartButton(i)).click();
+    }
+    expect(await inventoryPage.getShoppingCartBadgeCount()).toBe(6);
+
+    // Remove products from the cart
+    for (let i = 5; i >= 0; i--) {
+      await (await inventoryPage.getAddToCartButton(i)).click();
+      expect(await inventoryPage.getShoppingCartBadgeCount()).toBe(i);
     }
   });
 });
