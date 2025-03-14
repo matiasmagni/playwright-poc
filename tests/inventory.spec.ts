4ce905a3d86590b1e70f072f3503fbfe7d939964
Matias J. Magni <matias.magni@gmail.com>
2025-03-14 15:57:53 -0300

    Merge 2 tests from Inventory test suite in only one.

--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -2,19 +2,7 @@ import { test, expect } from '@playwright/test';
 import { InventoryPage } from './pages/InventoryPage';
 
 test.describe('Inventory Test Suite', () => {
-  test('Should increase products count in shopping cart badge when adding', async ({ page }) => {
-    const inventoryPage = new InventoryPage(page);
-    await inventoryPage.goto();
-    expect(await inventoryPage.getShoppingCartBadgeCount()).toBe(0);
-
-    for (let i = 0; i < 6; i++) {
-      expect(await inventoryPage.getInventoryItem(i)).toBeVisible();
-      await (await inventoryPage.getAddToCartButton(i)).click();
-      expect(await inventoryPage.getShoppingCartBadgeCount()).toBe(i + 1);
-    }
-  });
-  
-  test('Should decrease products count in shopping cart badge when removing', async ({ page }) => {
+  test('Should modify products count in shopping cart badge', async ({ page }) => {
     const inventoryPage = new InventoryPage(page);
     await inventoryPage.goto();
     expect(await inventoryPage.getShoppingCartBadgeCount()).toBe(0);
